d8de9ec56c946191d5d99c937d7d755df8a27e19
frank-iia <frank.mwema@investinafrica.com>
2020-04-15 02:00:21 +0300

    Challenge 1-5

--- src/estimator.php
+++ src/estimator.php
@@ -1,91 +1,118 @@
+
+
+
+Skip to content
+Using Gmail with screen readers
+
+1 of 3,255
+(no subject)
+Inbox
+x
+
+alfred musyoka
+Attachments
+1:53 AM (2 minutes ago)
+to me
+
+
+Kind Regards,
+
+Alfred  Musyoka
+
+
+FULL STACK DEVELOPER 
+
+Mobile Safaricom:  (+254) 0711 359 336
+
+Mobile Airtel: (+254) 0733 147 895
+
+Attachments area
+Well received, thanks.Received, thank you.Thanks, received.
+
 <?php
 
 function covid19ImpactEstimator($data)
 {
-  
-  readInputData($data);
-  
+	readInputData($data);	
 }
+
 function readInputData($data){
 
-  $input[] = json_decode($data);
- foreach ($input as $value) {
-   $reportedCases = $value->reportedCases;
-   $periodType = $value->periodType;
-   $timeToElapse = $value->timeToElapse;
-   $totalHospitalBeds = $value->totalHospitalBeds;
-   //$population = $value->population;
- }
- currentlyInfected($reportedCases);
- impactInfectionsByRequestedTime($reportedCases, $periodType, $timeToElapse);
- severeImpactInfectionsByRequestedTime($reportedCases, $periodType, $timeToElapse);
- severeCasesByRequestedTime($reportedCases,$periodType, $timeToElapse);
- severeImpactInfectionsByRequestedTime($reportedCases,$periodType, $timeToElapse);
- hospitalBedsByRequestedTime($totalHospitalBeds, $reportedCases,$periodType, $timeToElapse);
- casesForICUByRequestedTime($reportedCases,$periodType, $timeToElapse);
- casesForVentilatorsByRequestedTime($reportedCases, $periodType, $timeToElapse);
- dollarsInFlight($reportedCases,$periodType, $timeToElapse);
+   $input[] = json_decode($data);
+	foreach ($input as $value) {
+		$reportedCases = $value->reportedCases;
+		$periodType = $value->periodType;
+		$timeToElapse = $value->timeToElapse;
+		$totalHospitalBeds = $value->totalHospitalBeds;
+	}
+	currentlyInfected($reportedCases);
+	impactInfectionsByRequestedTime($reportedCases, $periodType, $timeToElapse);
+	severeImpactInfectionsByRequestedTime($reportedCases, $periodType, $timeToElapse);
+	severeCasesByRequestedTime($reportedCases,$periodType, $timeToElapse);
+	severeImpactInfectionsByRequestedTime($reportedCases,$periodType, $timeToElapse);
+	hospitalBedsByRequestedTime($totalHospitalBeds, $reportedCases,$periodType, $timeToElapse);
+ 	casesForICUByRequestedTime($reportedCases,$periodType, $timeToElapse);
+ 	casesForVentilatorsByRequestedTime($reportedCases, $periodType, $timeToElapse);
+ 	dollarsInFlight($reportedCases,$periodType, $timeToElapse);
+ 	jsonApi($data,$totalHospitalBeds,$reportedCases, $periodType, $timeToElapse);
 }
 
 function currentlyInfected($reportedCases){
- return $reportedCases * 10;
+	return $reportedCases * 10;
 }
 
 function severeCurrentlyInfected($reportedCases){
- return $reportedCases * 50;
+	return $reportedCases * 50;
 }
 
 function infectionsByRequestedTime($periodType, $timeToElapse){
- if (strtolower($periodType) == "days") {
-   $exponential = (int)($timeToElapse / 3);
-   return pow(2,$exponential);	
- }
- elseif (strtolower($periodType) == "weeks") {
-   $noOfDays = (int)($timeToElapse * 7);
-   $exponential = (int)($noOfDays / 3);
-   return pow(2,$exponential);	
- }
- elseif (strtolower($periodType) == "months") {
-   $noOfDays = (int)($timeToElapse * 30);
-   $exponential = (int)($noOfDays / 3);
-   return pow(2,$exponential);	
- }
- else {
-   return "Invalid Period Type.";
- }
+	if (strtolower($periodType) == "days") {
+		$exponential = (int)($timeToElapse / 3);
+		return pow(2,$exponential);	
+	}
+	elseif (strtolower($periodType) == "weeks") {
+		$noOfDays = (int)($timeToElapse * 7);
+		$exponential = (int)($noOfDays / 3);
+		return pow(2,$exponential);	
+	}
+	elseif (strtolower($periodType) == "months") {
+		$noOfDays = (int)($timeToElapse * 30);
+		$exponential = (int)($noOfDays / 3);
+		return pow(2,$exponential);	
+	}
+	else {
+		return "Invalid Period Type.";
+	}
 }
 
 function impactInfectionsByRequestedTime($reportedCases,$periodType, $timeToElapse){
- return currentlyInfected($reportedCases) * infectionsByRequestedTime($periodType, $timeToElapse);
+	return currentlyInfected($reportedCases) * infectionsByRequestedTime($periodType, $timeToElapse);
 }
 
 function severeImpactInfectionsByRequestedTime($reportedCases,$periodType, $timeToElapse){
- return severeCurrentlyInfected($reportedCases) * infectionsByRequestedTime($periodType, $timeToElapse);
+	return severeCurrentlyInfected($reportedCases) * infectionsByRequestedTime($periodType, $timeToElapse);
 }
 
 function severeCasesByRequestedTime($reportedCases,$periodType, $timeToElapse){
- $impact = impactInfectionsByRequestedTime($reportedCases,$periodType, $timeToElapse) * 0.15;
- $severe = severeImpactInfectionsByRequestedTime($reportedCases,$periodType, $timeToElapse) * 0.15;
- $combined =["impact"=>(int)$impact,"severe"=>(int)$severe];
- return json_encode($combined);
+	$impact = impactInfectionsByRequestedTime($reportedCases,$periodType, $timeToElapse) * 0.15;
+	$severe = severeImpactInfectionsByRequestedTime($reportedCases,$periodType, $timeToElapse) * 0.15;
+	$combined =["impact"=>(int)$impact,"severe"=>(int)$severe];
+	return json_encode($combined);
 }
 
 function hospitalBedsByRequestedTime($totalHospitalBeds, $reportedCases,$periodType, $timeToElapse){
- $totalBeds = $totalHospitalBeds * 0.35;
- $combined = severeCasesByRequestedTime($reportedCases,$periodType, $timeToElapse);
- $data[] = json_decode($combined);
- foreach ($data as $value) {
-   $impact = $value->impact;
-   $severe = $value->severe;
- }
- $impactBeds = $totalBeds - $impact;
- $severeBeds = $totalBeds - $severe;
- $bedsHistory =["impactBeds"=>(int)$impactBeds,"severeBeds"=>(int)$severeBeds];
- return json_encode($bedsHistory);
- //return (int)$totalBeds;
- //print_r(json_encode($bedsHistory));
+	$totalBeds = $totalHospitalBeds * 0.35;
+	$combined = severeCasesByRequestedTime($reportedCases,$periodType, $timeToElapse);
+	$data[] = json_decode($combined);
+	foreach ($data as $value) {
+		$impact = $value->impact;
+		$severe = $value->severe;
+	}
+	$impactBeds = $totalBeds - $impact;
+	$severeBeds = $totalBeds - $severe;
+	$bedsHistory =["impactBeds"=>(int)$impactBeds,"severeBeds"=>(int)$severeBeds];
+	return json_encode($bedsHistory);
 }
-
 function casesForICUByRequestedTime($reportedCases, $periodType, $timeToElapse){
  $impactICU = impactInfectionsByRequestedTime($reportedCases, $periodType, $timeToElapse) * 0.05;
  $severeICU = severeImpactInfectionsByRequestedTime($reportedCases, $periodType, $timeToElapse) * 0.05; 
@@ -101,30 +128,90 @@ function casesForVentilatorsByRequestedTime($reportedCases, $periodType, $timeTo
 }
 
 function dollarsInFlight($reportedCases,$periodType, $timeToElapse){
- $combined = severeCasesByRequestedTime($reportedCases,$periodType, $timeToElapse);
- $data[] = json_decode($combined);
- foreach ($data as $value) {
-   $impact = $value->impact;
-   $severe = $value->severe;
-}
-$dollarsImpact = ($impact * 0.65 * 1.5) / 30;
-$dollarsSevere = ($severe * 0.65 * 1.5) / 30;
+$impact = impactInfectionsByRequestedTime($reportedCases,$periodType, $timeToElapse);
+$severe = severeImpactInfectionsByRequestedTime($reportedCases,$periodType, $timeToElapse);
+$dollarsImpact = ($impact * 0.65 * 4 * 0.73);
+$dollarsSevere = ($severe * 0.65 * 4 * 0.73);
 $combinedDollars =["dollarsImpact"=>(int)$dollarsImpact,"dollarsSevere"=>(int)$dollarsSevere];
-echo json_encode($combinedDollars);
+return json_encode($combinedDollars);
 }
 
- 
+function jsonApi($data,$totalHospitalBeds,$reportedCases, $periodType, $timeToElapse){
+	$sBT = severeCasesByRequestedTime($reportedCases,$periodType, $timeToElapse);
+	$input[] = json_decode($sBT);
+	foreach ($input as $value) {
+		$impactSevereCasesByRequestedTime = $value->impact;
+		$severeCasesByRequestedTime = $value->severe;
+	}
+
+	$hosy = hospitalBedsByRequestedTime($totalHospitalBeds, $reportedCases,$periodType, $timeToElapse);
+	$hospital[] = json_decode($hosy);
+	foreach ($hospital as $value) {
+		$impactBeds = $value->impactBeds;
+	    $severeBeds = $value->severeBeds;
+	}
+
+	$icu = casesForICUByRequestedTime($reportedCases, $periodType, $timeToElapse);
+	$iCU[] = json_decode($icu);
+	foreach ($iCU as $value) {
+		$impactICU = $value->impactICU;
+	    $severeICU = $value->severeICU;
+	}
+
+	$vents = casesForVentilatorsByRequestedTime($reportedCases, $periodType, $timeToElapse);
+	$vENTS[] = json_decode($vents);
+	foreach ($vENTS as $value) {
+		$impactVents = $value->impactVents;
+	    $severeVents = $value->severeVents;
+	}
+
+	$dolla = dollarsInFlight($reportedCases,$periodType, $timeToElapse);
+	$dOLLA[] = json_decode($dolla);
+	foreach ($dOLLA as $value) {
+		$dollarsImpact = $value->dollarsImpact;
+	    $dollarsSevere = $value->dollarsSevere;
+	}
+    
+	    $impact = [
+		 	    "currentlyInfected" =>currentlyInfected($reportedCases),
+				"infectionsByRequestedTime" =>impactInfectionsByRequestedTime($reportedCases,$periodType, $timeToElapse),
+				"severeCasesByRequestedTime" =>$impactSevereCasesByRequestedTime,
+				"hospitalBedsByRequestedTime" => $impactBeds,
+				"casesForICUByRequestedTime" =>$impactICU,
+				"casesForVentilatorsByRequestedTime" =>$impactVents,
+				"dollarsInFlight" =>$dollarsImpact
+			  ];
+
+        $severeImpact = [
+		 	    "currentlyInfected" =>severeCurrentlyInfected($reportedCases),
+				"infectionsByRequestedTime" =>severeImpactInfectionsByRequestedTime($reportedCases,$periodType, $timeToElapse),
+				"severeCasesByRequestedTime" =>$severeCasesByRequestedTime,
+				"hospitalBedsByRequestedTime" =>$severeBeds,
+				"casesForICUByRequestedTime" =>$severeICU,
+				"casesForVentilatorsByRequestedTime" =>$severeVents,
+				"dollarsInFlight" =>$dollarsSevere
+			  ];	
+
+		 $final =["impact"=>$impact,"severeImpact"=>$severeImpact];
+
+		 $restApi = (object)array();
+		 $restApi->data = json_decode($data);
+		 $restApi->estimate = json_decode(json_encode($final));
+		 print_r(json_encode($restApi));
+
+}
+
 $data = '{
-     "region": {
-       "name": "Africa",
-       "avgAge": 19.7,
-       "avgDailyIncomeInUSD": 5,
-       "avgDailyIncomePopulation": 0.71
-     },
-     "periodType": "days",
-     "timeToElapse": 38,
-     "reportedCases": 2747,
-     "population": 92931687,
-     "totalHospitalBeds": 678874
-   }';
+			"region": {
+				"name": "Africa",
+				"avgAge": 19.7,
+				"avgDailyIncomeInUSD": 5,
+				"avgDailyIncomePopulation": 0.71
+			},
+			"periodType": "days",
+			"timeToElapse": 38,
+			"reportedCases": 2747,
+			"population": 66622705,
+			"totalHospitalBeds": 678874
+		}';
 covid19ImpactEstimator($data);
